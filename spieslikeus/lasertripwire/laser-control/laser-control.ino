03d68a2d3e2e3dc76526636e13be9a229dbe71af
Peter Januarius <pete@nexgencodecamp.com.au>
2018-10-04 18:25:23 +1000

    added project files

---
+++ spieslikeus/lasertripwire/laser-control/laser-control.ino
@@ -0,0 +1,13 @@
+// Project 1 - Laser Controller
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+  // initialize digital pin LED_BUILTIN as an output.
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, HIGH);
+}
+
+// the loop function runs over and over again forever
+void loop() {
+  // Nothing here folks !
+}
